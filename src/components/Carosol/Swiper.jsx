522f751c6178df4426775c58f7c884e1687663a0
Md. Minhaz Uddin <ni.xeron07@gmail.com>
2024-06-04 03:40:22 +0600

    update | home page updated

---
+++ src/components/Carosol/Swiper.jsx
@@ -0,0 +1,46 @@
+"use client";
+import { useEffect, useRef } from "react";
+import SwiperCore from "swiper";
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import Image from "next/image";
+
+SwiperCore.use([Autoplay]);
+
+//@ts-ignore
+const Carousel = ({ images, direction = "horizontal", delay = 3000 }) => {
+  return (
+    <Swiper
+      centeredSlides={true}
+      pagination={{
+        clickable: true,
+      }}
+      navigation={true}
+      modules={[Autoplay, Pagination, Navigation]}
+      spaceBetween={0}
+      direction={direction}
+      slidesPerView={1}
+      loop={true}
+      autoplay={{
+        delay: delay,
+        disableOnInteraction: false,
+      }}
+      className='w-full h-full'>
+      {images.map((image, index) => (
+        <SwiperSlide key={index}>
+          <Image
+            src={image}
+            alt={`Image ${index}`}
+            height={800}
+            width={200}
+            quality={100}
+            className='w-full object-fill rounded-3xl'
+          />
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
+};
+
+export default Carousel;
