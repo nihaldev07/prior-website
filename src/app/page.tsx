126894f0b2a34779ed2dfc8e82b1d7c7996565d3
Md. Minhaz Uddin <ni.xeron07@gmail.com>
2024-10-22 19:37:34 +0600

    update | new patch released 0.3.2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,8 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { config } from "@/lib/config";
 import HomePage from "@/components/pages/Home";
-import Image from "next/image";
 
 export default function Home() {
-  const [loading, setLoading] = useState(true); // For loading state
   const [categories, setCategories] = useState([]);
   const [products, setProducts] = useState([]);
   const [error, setError] = useState(false);
@@ -15,7 +13,6 @@ export default function Home() {
     const fetchData = async () => {
       try {
         // Start loading
-        setLoading(true);
         const [categoryResponse, productResponse] = await Promise.all([
           axios.get(config.product.getCategories()),
           axios.get(config.product.getNewProducts()),
@@ -34,29 +31,11 @@ export default function Home() {
         console.error("Error fetching data:", error);
         setError(true); // Handle error state
       } finally {
-        setLoading(false); // Stop loading
       }
     };
 
     fetchData();
   }, []);
-
-  // Show loading state
-  if (loading) {
-    return (
-      <main className="flex min-h-[50vh] flex-col items-center justify-center">
-        <div className="flex justify-center items-center flex-col animate-out">
-          <Image
-            src="https://prior-image.s3.eu-north-1.amazonaws.com/cat-chase-loading-animation.gif"
-            width={512}
-            height={512}
-            alt="loading..."
-            unoptimized
-          />
-        </div>
-      </main>
-    );
-  }
 
   // Show error message if fetch fails
   if (error) {
