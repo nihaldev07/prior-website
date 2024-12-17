a8fb73d1f76a65ef0fb7f16b50c01e9449edc4ef
Md. Minhaz Uddin <ni.xeron07@gmail.com>
2024-12-17 18:36:02 +0600

    update : gtm added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import Navbar from "./navbar";
 import Footer from "@/shared/Footer/Footer";
 // import Maintenance from "./Maintainance";
 import { PageStateProvider } from "@/context/PageStateContext";
+import Script from "next/script";
 
 const inter = Poppins({
   weight: "400",
@@ -49,7 +50,32 @@ export default function RootLayout({
   //useAnalytics();
   return (
     <html lang="en">
+      <head>
+        {/* Google Tag Manager - head script */}
+        <Script
+          id="google-tag-manager"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-T2HZLQ22');
+            `,
+          }}
+        />
+      </head>
       <body className={inter.className}>
+        {/* Google Tag Manager - noscript fallback */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-T2HZLQ22"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <PageStateProvider>
           <CartProvider>
             <>
